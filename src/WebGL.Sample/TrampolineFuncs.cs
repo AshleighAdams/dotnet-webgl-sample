69b04f49020e411ed88969ad233e6f88f730c89c
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2024-07-26 00:39:49 +0100

    Some AOT compilation changes

--- src/WebGL.Sample/TrampolineFuncs.cs
+++ src/WebGL.Sample/TrampolineFuncs.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 
@@ -9,9 +10,11 @@ namespace WebGL.Sample;
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "OpenGL names")]
 internal unsafe static class TrampolineFuncs
 {
+	[DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(TrampolineFuncs))]
 	public static void ApplyWorkaroundFixingInvocations()
 	{
-		Marshal.GetDelegateForFunctionPointer(new nint(1), typeof(glClear_t));
+		Marshal.GetDelegateForFunctionPointer<glClear_t>(nint.Zero);
+		Marshal.GetDelegateForFunctionPointer<glClearColor_t>(nint.Zero);
 	}
 
 	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
